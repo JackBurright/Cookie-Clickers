57903a81f553dfe7cf1b135c247efca007ea45ba
JackBurright <Jrb94233@creighton.edu>
2024-06-25 23:05:57 -0500

    first commit

---
+++ selenium-automation.py
@@ -0,0 +1,76 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import re
+
+driver = webdriver.Chrome()
+wait2 = WebDriverWait(driver, 2)
+wait10 = WebDriverWait(driver, 10)
+
+
+def clickCookie(num):
+    cookie_to_click = wait10.until(EC.element_to_be_clickable((By.ID, 'bigCookie')))
+    for _ in range(num):
+        cookie_to_click.click()
+    return 
+
+def clickLanguage():
+    try:
+        language = driver.find_element(By.ID, 'langSelect-EN')
+        language.click()
+        return True
+    except:
+        return False
+
+def getNumCookies():
+    num_cookies_title =  wait10.until(EC.element_to_be_clickable((By.ID, 'cookies')))
+    num_cookies = int(re.findall(r'\d+', num_cookies_title.text)[0])
+    return num_cookies
+
+def buyCursor(num):
+    buy_cursor = driver.find_element(By.ID, 'product0')
+    for _ in range(num):
+        buy_cursor.click()
+    return
+
+def priceCursor():
+    click_price = driver.find_element(By.ID, 'productPrice0')
+    click_price_int = int(click_price.text)
+    return click_price_int
+
+def buyGrandma(num):
+    buy_grandma = driver.find_element(By.ID, 'product1')
+    for _ in range(num):
+        buy_grandma.click()
+    return
+
+def priceGrandma():
+    grandma_price = driver.find_element(By.ID, 'productPrice1')
+    grandma_price_int = int(grandma_price.text) 
+    return grandma_price_int
+
+def buyUpgrade():
+    next_upgrade = wait2.until(EC.element_to_be_clickable((By.ID, 'upgrade0')))
+    class_attribute = next_upgrade.get_attribute("class")
+    if "enabled" in class_attribute.split():
+        next_upgrade.click()
+        return True
+    return False
+
+
+driver.get('https://orteil.dashnet.org/cookieclicker/')
+time.sleep(1)
+if not clickLanguage():
+    time.sleep(3)
+    if not clickLanguage():
+        raise AssertionError
+
+time.sleep(1)
+clickCookie(30)
+
+
+
+time.sleep(5)
+driver.quit()
